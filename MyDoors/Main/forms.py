417d0efe2b985fcc526c741884723898c6d91652
Arfeil <dumeinck.eldos@gmail.com>
2023-09-17 01:42:40 +0600

    bot run

--- MyDoors/Main/forms.py
+++ MyDoors/Main/forms.py
@@ -5,3 +5,8 @@ class DoorsForm(forms.ModelForm):
     class Meta:
         model = Doors
         fields = ['shape', 'color', 'molding', 'portal']
+
+    shape = forms.ChoiceField(choices=Doors.SHAPE_CHOICES)
+    color = forms.ChoiceField(choices=Doors.COLOR_CHOICES)
+    molding = forms.ChoiceField(choices=Doors.MOLDING_CHOICES)
+    portal = forms.ChoiceField(choices=Doors.PORTAL_CHOICES)
--- MyDoors/Main/models.py
+++ MyDoors/Main/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 
 class Doors(models.Model):
-    shape = models.CharField(max_length=255)
-    color = models.CharField(max_length=255)
-    molding = models.CharField(max_length=255)
-    portal = models.CharField(max_length=255)
+    SHAPE_CHOICES = [
+        ('none', 'None'),
+    ]
+
+    COLOR_CHOICES = [
+        ('none', 'None'),
+    ]
+
+    MOLDING_CHOICES = [
+        ('none', 'None'),
+    ]
+
+    PORTAL_CHOICES = [
+        ('none', 'None'),
+    ]
+
+    shape = models.CharField(max_length=100, choices=SHAPE_CHOICES)
+    color = models.CharField(max_length=100, choices=COLOR_CHOICES)
+    molding = models.CharField(max_length=100, choices=MOLDING_CHOICES)
+    portal = models.CharField(max_length=100, choices=PORTAL_CHOICES)
+
+    # Другие поля и методы модели...
